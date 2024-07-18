972db1ea32dc71daafb9a5dddc0dd7126f61074b
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-18 17:17:46 +0700

    home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,28 +1,19 @@
 import React from "react";
-import video from "../assets/images/shop.gif";
-import "../assets/styles/pages/_home.scss";
+import HeaderPage from "./HeaderPage";
+import Carousel from "../components/banner-slide/Carousel";
+import OptionPromo from "../components/option-promo/OptionPromo";
+import ProductsPage from "./ProductsPage";
 
 const HomePage = () => {
   return (
-    <div className="home-page row">
-      <div className="home-page__title col-12 col-md-6 ">
-        <p className="title-head">Whatever you need, we are always ready!</p>
-        <p className="title-body">
-          Here, we always provide the best and newest products to you. Products
-          are always updated and delivered to you quickly.
-        </p>
-        <button className="btn btn-outline-dark btn-lg">GET START</button>
+    <main>
+      <HeaderPage />
+      <div className="main-content container">
+        <Carousel />
+        <OptionPromo />
+        <ProductsPage />
       </div>
-      <div className="home-page__video col-12 col-md-6 ">
-        <img src={video} alt="shop" className="video-content" />
-        <div className="video-overlay">
-          <p className="overlay-text">
-            Whatever you need, we are always ready!
-          </p>
-          <button className="btn btn-dark btn-lg overlay-btn">GET START</button>
-        </div>
-      </div>
-    </div>
+    </main>
   );
 };
 
