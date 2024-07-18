fa6450f696cab9530143fdb1a748bd7a45f0ac35
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-18 17:17:09 +0700

    products page

---
+++ client/src/pages/ProductsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductPromo from "../components/products/product-promo/ProductPromo";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <ProductPromo />
+    </div>
+  );
+};
+
+export default ProductsPage;
