8f308a94e82e1267c24c3790400ff5713c8aa547
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-28 18:26:42 +0700

    product

---
+++ client/src/components/admin/content/menu/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TableProduct from "../card-item/products/TableProduct";
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Product management page</h1>
+      <TableProduct />
+    </div>
+  );
+};
+
+export default Products;
