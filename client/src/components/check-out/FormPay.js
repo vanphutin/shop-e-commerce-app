7a8c89c3cdd060116b6d0f6f0b5cf13b5b42b32b
vanphutin2004 <vanphutin.contact@gmail.com>
2024-08-21 15:09:50 +0700

    check out -degin

--- client/src/components/check-out/FormPay.js
+++ client/src/components/check-out/FormPay.js
@@ -1,23 +1,50 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { CiMoneyCheck1 } from "react-icons/ci";
 
-function BasicExample() {
+function FormPay() {
   return (
-    <Form className="row">
-      <Form.Group className="mb-3 col-6" controlId="formBasicEmailPhone">
-        <Form.Label>Email or Phone</Form.Label>
-        <Form.Control type="text" placeholder="Enter email or phone" />
-      </Form.Group>
-      <Form.Group className="mb-3 col-6" controlId="formBasicFullName">
-        <Form.Label>Full name</Form.Label>
-        <Form.Control type="text" placeholder="Enter full name" />
-      </Form.Group>
-      <Form.Group className="mb-3 col-12" controlId="formBasicAddress">
-        <Form.Label>Address</Form.Label>
-        <Form.Control type="text" placeholder="Enter address" />
-      </Form.Group>
+    <Form className="">
+      <div className="form__details row">
+        <h6 className="title fw-bolder my-2">Enter info details</h6>
+        <Form.Group className="mb-3 col-6" controlId="formBasicEmailPhone">
+          <Form.Label>Email or Phone</Form.Label>
+          <Form.Control type="text" placeholder="Enter email or phone" />
+        </Form.Group>
+        <Form.Group className="mb-3 col-6" controlId="formBasicFullName">
+          <Form.Label>Full name</Form.Label>
+          <Form.Control type="text" placeholder="Enter full name" />
+        </Form.Group>
+        <Form.Group className="mb-3 col-12" controlId="formBasicAddress">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="Enter address" />
+        </Form.Group>
+      </div>
+      <div className="form__pay">
+        <h6 className="title fw-bolder my-2">Enter info card</h6>
+        <Form.Group className="mb-3 col-12" controlId="formBasicAccountBanking">
+          <Form.Label>Banking </Form.Label>
+          <Form.Control
+            type="text"
+            className="text-uppercase"
+            placeholder="MBBANK"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 col-12" controlId="formBasicAccountNumber">
+          <Form.Label>Account number</Form.Label>
+          <Form.Control type="text" placeholder="123456" />
+        </Form.Group>
+        <Form.Group className="mb-3 col-12" controlId="formBasicAccounNamet">
+          <Form.Label>Name account</Form.Label>
+          <Form.Control
+            type="text"
+            className="text-uppercase"
+            placeholder="JOD AN"
+          />
+        </Form.Group>
+      </div>
     </Form>
   );
 }
 
-export default BasicExample;
+export default FormPay;
--- client/src/components/check-out/Pay.js
+++ client/src/components/check-out/Pay.js
@@ -13,68 +13,89 @@ const Pay = () => {
   const location = useLocation();
   const { id } = location.state || {};
   const { totalPrice } = location.state || {};
-  console.log(totalPrice);
+  const { product } = location.state || {};
 
   useEffect(() => {
-    fetchCartItems(id);
-  }, []);
+    if (id) {
+      fetchCartItems(id);
+    }
+  }, [id]);
   const fetchCartItems = async (id) => {
     try {
       const res = await getCartItems(id);
       if (res.code !== 200) {
         return toast.error("Error server !");
       }
-      setProductItem(res.data);
+
+      setProductItem(id ? res.data : product);
     } catch (error) {
       console.log("error", error);
     }
   };
+  const ulStyle = {
+    listStyle: "none",
+    height: "560px",
+    overflowY: "scroll",
+  };
   return (
-    <div className="container pay ">
+    <div className="container pay mt-3 ">
       <div className="pay__container row">
-        <div className="pay__product col-6">
-          <div className="pay__product-back">
-            <h5 className="back-to-cart">
-              {" "}
-              <IoChevronBackOutline />
-              Back to cart
-            </h5>
-          </div>
+        <h5 className="back-to-cart fs-5">
+          <IoChevronBackOutline />
+          Back to cart
+        </h5>
+        <div className="pay__product col-6 bg-secondary text-white">
+          <div className="pay__product-back"></div>
           <div className="pay__product-title">
-            <h5 className="title">Products infomatin & review</h5>
+            <h5 className="title mt-1">
+              Products infomatin & review{" "}
+              <b className="mx-5"> {productItem?.length || 1} items</b>
+            </h5>
+            <hr />
           </div>
-          <ul className="pay__product-item p-0 lis">
-            {productItem &&
+          <ul className="pay__product-item p-0 lis" style={ulStyle}>
+            {productItem && id ? (
               productItem.map((items, index) => (
-                <li className="pay__product-items" key={index}>
+                <li className="pay__product-items mb-2" key={index}>
                   <ProductCard
-                    productCart={items}
+                    productCart={id ? items : product}
                     mainclassName="row"
                     imageclassName="col-4"
                     detailclassName="col-8"
                     sizeImg="500"
+                    Linkto="/products/detail"
                   />
                 </li>
-              ))}
+              ))
+            ) : (
+              <li className="pay__product-items mb-2" key={product.ProductID}>
+                <ProductCard
+                  productCart={product}
+                  mainclassName="row"
+                  imageclassName="col-4"
+                  detailclassName="col-8"
+                  sizeImg="500"
+                  Linkto="/products/detail"
+                />
+              </li>
+            )}
           </ul>
         </div>
-        <div className="pay__info col-6">
+        <div className="pay__info col-6 " style={{ paddingLeft: "60px" }}>
           <div className="pay__product-back pay__info-detail">
-            <h5 className="back-to-cart">Payment details</h5>
+            <h5 className="back-to-cart my-2 text-center fs-2">
+              Payment details
+            </h5>
           </div>
           <div className="pay__product-form">
-            <FormPay />
+            <FormPay price={product?.ProductID} />
           </div>
-          <div className="pay__product-pay-method">
-            <div className="pay-method">
-              <div className="pay-method-icon">
-                <CiMoneyCheck1 />
-              </div>
-              <div className="pay-method-text">Debit / Credit Card</div>
-            </div>
+          <div className="pay__product-check-out  ">
+            <button className="btn btn-dark w-100 ">
+              Pay ${totalPrice ? totalPrice : product?.ProductPrice || NaN}
+            </button>
           </div>
         </div>
-        <h1>Form</h1>
       </div>
     </div>
   );
