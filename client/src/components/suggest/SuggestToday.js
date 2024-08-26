373461e8fe1cc5f8c35c5cffd460d5e259ad864e
vanphutin2004 <vanphutin.contact@gmail.com>
2024-08-26 10:32:58 +0700

    loading when wait

--- client/src/components/suggest/SuggestToday.js
+++ client/src/components/suggest/SuggestToday.js
@@ -1,42 +1,90 @@
 import React, { useContext, useEffect, useState } from "react";
 import ProductCard from "../products/product-card/ProductCard";
 import "../../assets/styles/components/sugges/__SuggestToday.scss";
-import headephone from "../../assets/images/product-promo/headphone.webp";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { getAllProducts } from "../../services/apiServerviceProduct";
-import { AuthContext } from "../../context/AuthProvider";
-import { postAddCart } from "../../services/apiServerviceCart";
 var Loader = require("react-loaders").Loader;
 
 const SuggestToday = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [fakeProductLoading, setFakeLoadingProduct] = useState([]);
 
   useEffect(() => {
     getProduct();
+    fakeProduct();
   }, []);
+
   const getProduct = async () => {
     try {
       const res = await getAllProducts();
       if (res.code !== 200) {
-        return toast.error("error");
+        toast.error("Error fetching products");
+        setLoading(false); // Stop loading if there's an error
+        return;
       }
       setProducts(res.data);
     } catch (error) {
       console.log(error);
+      toast.error("An error occurred");
+    } finally {
+      setLoading(false); // Stop loading after data fetching is done
     }
+  };
+  const fakeProduct = () => {
+    const fakeProducts = [];
+    for (let i = 0; i <= 7; i++) {
+      fakeProducts.push({
+        ProductID: i,
+        ProductName: "",
+        ProductPrice: "",
+        ProductWeight: "",
+        ProductLongDesc: "",
+        ProductImage: "",
+        ProductCategoryID: "1",
+        ProductUpdateDate: "2024-08-21T11:32:25.000Z",
+        ProductStock: "",
+        Deleted: "",
+        UserID: "1",
+        createdAt: "",
+        updatedAt: "2024-08-21T11:32:25.000Z",
+        CategoryName: "",
+      });
+    }
+    setFakeLoadingProduct(fakeProducts);
   };
 
   return (
     <div className="suggest-today">
-      <div className="suggest-today__header featured-categories__header deal-card__header ">
+      <div className="suggest-today__header featured-categories__header deal-card__header">
         <div className="header-title text-center">Suggest Today</div>
       </div>
       <div className="suggest-today__main row">
-        {products ? (
+        {loading ? (
+          <>
+            <Loader type="line-scale" active />
+            <p className="text-center ">Loading ...</p>
+            {fakeProductLoading.map((items, index) => (
+              <div
+                className="main-items main-items-loading  col-12 col-md-4 col-sm-5 col-lg-3 gy-3"
+                key={`${items.id}-${index}`}
+              >
+                <ProductCard
+                  productCart={items}
+                  mainclassName="row"
+                  imageclassName="col-12"
+                  detailclassName="col-12"
+                  showButton={false}
+                  Linkto="products/detail"
+                />
+              </div>
+            ))}
+          </>
+        ) : (
           products.slice(0, 20).map((product, index) => (
             <div
-              className="main-items col-12 col-md-4 col-sm-5 col-lg-3 gy-3 "
+              className="main-items col-12 col-md-4 col-sm-5 col-lg-3 gy-3"
               key={`${product.id}-${index}`}
             >
               <ProductCard
@@ -49,8 +97,6 @@ const SuggestToday = () => {
               />
             </div>
           ))
-        ) : (
-          <Loader type="line-scale" active />
         )}
       </div>
       <div className="suggest-today__footer text-center">
@@ -59,8 +105,6 @@ const SuggestToday = () => {
             View All Products
           </button>
         </Link>
-
-        {/* link to all products page */}
       </div>
     </div>
   );
