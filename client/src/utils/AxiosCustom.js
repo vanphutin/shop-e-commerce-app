3e2c6b6c536d65a1809b876a09a7f92f1538e6ee
vanphutin2004 <vanphutin.contact@gmail.com>
2024-11-26 01:00:55 +0700

    cors v1

--- client/src/utils/AxiosCustom.js
+++ client/src/utils/AxiosCustom.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  // baseURL: "https://shop-e-commerce-app.onrender.com/api/v1",
-  baseURL: "http://localhost:8081/api/v1/",
+  baseURL: "https://shop-e-commerce-app.onrender.com/api/v1",
+  // baseURL: "http://localhost:8081/api/v1/",
   //   timeout: 1000,
   //   headers: { "X-Custom-Header": "foobar" },
 });
